03e75dce0be655ea5eaa65633f48609f691497c0
Philipp Krähenbühl <philkr@users.noreply.github.com>
2020-12-28 08:51:02 -0600

    Update replay.py

--- carla_utils/recording/replay.py
+++ carla_utils/recording/replay.py
@@ -50,14 +50,7 @@ def replay(client, recording: str, fps: int = 10):
         settings.synchronous_mode = False
         world.apply_settings(settings)
 
-        # Don't know why this doesn't work.
-        # client.stop_replayer will hang the server, every time
-        # client.stop_replayer(keep_actors=False)
-        # potential workaround make a tick infinitely long (does not work)
-        # client.set_replayer_time_factor(1000)
-        # world.tick()
-        # client.set_replayer_time_factor(1)
-
-        # There is an long loop for keep_actors=False, which may hang the server
+        # client.stop_replayer(keep_actors=False) will hang the server, every time.
+        # The carla server will parse the recording on keep_actors=False causing the client to timeout.
+        # The keep the actors here, but any new replay or scenario will purge them after.
         client.stop_replayer(keep_actors=True)
-        # TODO: Let's manually purge all actors
